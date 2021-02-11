a869c77d9b80860490ab890df7b80f4d3851114e
rlobz17 <rlobz17@freeuni.edu.ge>
2021-02-11 08:39:38 +0400

    updated banner

--- script.js
+++ script.js
@@ -21,3 +21,21 @@ times_button.addEventListener('click', function (){
         navigation_bar.classList.remove("show_navigation");
     }
 })
+
+/*
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI?q=taylor%20swift&pageNumber=1&pageSize=10&autoCorrect=true&fromPublishedDate=null&toPublishedDate=null",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "eedc981b57msh96e2bb005cd2468p1bfa18jsnaf705a644510",
+		"x-rapidapi-host": "contextualwebsearch-websearch-v1.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
+
+*/
